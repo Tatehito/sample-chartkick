026db6b3bba7245117a28c6d6f9ba00483ef7e4b
Tatehito <tatehito.st@gmail.com>
2019-10-13 22:56:43 +0900

    Install Chartkick gem.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("chartkick")
+require("chart.js")
+require("chartkick").use(require("highcharts"))
